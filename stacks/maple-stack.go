79f8319bd64c35f3210c3ee6deca5c2d694951d3
Chase Livingston <chaseliv@gmail.com>
2025-10-20 16:07:04 -0600

    init commit

---
+++ stacks/maple-stack.go
@@ -0,0 +1,55 @@
+package stacks
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsecr"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsecs"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type CloudformationStackProps struct {
+	awscdk.StackProps
+}
+
+func MapleStackFormation(scope constructs.Construct, id string, props *CloudformationStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	// vpc for ecs cluster
+	vpc := awsec2.NewVpc(stack, jsii.String("MapleVPC"), &awsec2.VpcProps{
+		MaxAzs:      jsii.Number(2), // Use 2 availability zones
+		NatGateways: jsii.Number(1), // Use 1 NAT gateway to save costs
+	})
+
+	// ecr repo to store docker images
+	ecrRepo := awsecr.NewRepository(stack, jsii.String("MapleECRRepo"), &awsecr.RepositoryProps{
+		RemovalPolicy: awscdk.RemovalPolicy_DESTROY,
+	})
+	_ = ecrRepo
+
+	// ecs cluster, which is just the grouping of the ecs services
+	ecsCluster := awsecs.NewCluster(stack, jsii.String("MapleECSCluster"), &awsecs.ClusterProps{
+		Vpc: vpc,
+	})
+
+	// ecs task definition
+	ecsTaskDef := awsecs.NewTaskDefinition(stack, jsii.String("MapleECSTaskDef"), &awsecs.TaskDefinitionProps{
+		Compatibility: awsecs.Compatibility_FARGATE,
+		Cpu:           jsii.String("256"),
+		MemoryMiB:     jsii.String("512"),
+	})
+
+	// ecs service, what actually runs the containers
+	ecsService := awsecs.NewFargateService(stack, jsii.String("MapleECSService"), &awsecs.FargateServiceProps{
+		Cluster:        ecsCluster,
+		TaskDefinition: ecsTaskDef,
+	})
+	_ = ecsService
+
+	return stack
+}
